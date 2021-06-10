dd8f987d01c13b40ef3a8b178cb185209bf65459
Arshdeep <arshsingh3223@gmail.com>
2021-06-10 14:55:22 -0700

    Completed Assigment

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 100
+let b = 2000
+const divide = 
+function (){
+    return b/a;
+}
+
+const square = (parameter1) => parameter1*parameter1;
+
+const add = (meter1, meter2) => meter1+meter2;
+
